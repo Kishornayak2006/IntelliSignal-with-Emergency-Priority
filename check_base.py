8ca3f34c11c76c87411f3170820e382ec40a90d0
kishornayak2653@gmail.com <kishornayak2653@gmail.com>
2026-03-14 22:50:59 +0530

    updated

--- check_base.py
+++
@@ -1,8 +0,0 @@
-import torch
-import cv2
-from ultralytics import YOLO
-
-print("Torch:", torch.__version__)
-print("CUDA:", torch.cuda.is_available())
-print("OpenCV:", cv2.__version__)
-print("Ultralytics working")
